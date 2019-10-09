fddf25137ff8225fdba9918ffee81ebd6b82788a
Jeffrey Hutchins <jeffrey.w.hutchins@gmail.com>
2019-10-09 14:30:30 -0500

    Completed

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -16,6 +16,8 @@ from sklearn import metrics
 # The first step will be to acquire and prep the data. Do your work for this 
 # exercise in a file named wrangle.py.
 
+### USING EXCEL SPREADSHEET
+
 # 1. Acquire customer_id, monthly_charges, tenure, and total_charges from telco_churn 
 # database for all customers with a 2 year contract.
 
@@ -53,10 +55,57 @@ def wrangle_grades():
     data = pd.read_csv("Telco_Data.csv")
     data['tenure'] = round(data['Monthly_Tenure']/12, 2)
     data = data[['customer_id', 'monthly_charges', 'tenure', 'total_charges']]
-    
+
     # grades.drop(columns='student_id', inplace=True)
     data.replace(r'^\s*$', np.nan, regex=True, inplace=True)
     data = data.dropna().astype('int')
     return data
 
 
+### USING SQL DATABASE
+
+# 1. Acquire customer_id, monthly_charges, tenure, and total_charges from telco_churn 
+# database for all customers with a 2 year contract.
+
+import pandas as pd
+from env import user, host, password
+
+def get_db_url(user, host, password, database):
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    return url
+
+url = get_db_url(user, host, password, database="telco_churn")
+
+telco = pd.read_sql('''select customer_id, tenure, monthly_charges, total_charges
+from customers
+where contract_type_id = 3;'''
+, url)
+
+telco
+
+    # 2. Walk through the steps above using your new dataframe. You may handle the missing 
+# values however you feel is appropriate.
+
+
+telco.total_charges.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+telco.total_charges.dropna().astype('float')
+telco
+
+# 3. End with a python file wrangle.py that contains the function, wrangle_telco(), 
+# that will acquire the data and return a dataframe cleaned with no missing values.
+
+def get_db_url(user, host, password, database):
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    return url
+
+def wrangle_grades():
+    url = get_db_url(user, host, password, database="telco_churn")
+    telco = pd.read_sql('''select customer_id, tenure, monthly_charges, total_charges
+    from customers
+    where contract_type_id = 3;'''
+    , url)
+    telco.total_charges.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+    telco = telco.total_charges.dropna().astype('float')
+    return telco
+
+telco
