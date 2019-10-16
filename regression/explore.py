0268b615fe9b0d1ff735381f6fb957e634309e18
Jeffrey Hutchins <jeffrey.w.hutchins@gmail.com>
2019-10-16 12:25:01 -0500

    Visualization Functions

--- regression/explore.py
+++ regression/explore.py
@@ -21,20 +21,25 @@ import split_scale
 get_db_url(user, host, password, database="telco_churn")
 telco = wrangle_telco()
 telco
+df = telco
+df.head()
 
 # 1. Write a function, plot_variable_pairs(dataframe) that plots all of the pairwise 
 # relationships along with the regression line for each pair.
 
-def plot_variable_pairs(dataframe):
+def plot_variable_pairs(df):
+    g=sns.PairGrid(df)
+    g.map(sns.regplot)
+    plt.show()
 
 sns.PairGrid(telco)
 
 # 2. Write a function, months_to_years(tenure_months, df) that returns your dataframe with 
 # a new feature tenure_years, in complete years as a customer.
 
-def months_to_years(dataframe):
-   telco['tenure_years'] = telco.tenure/12
-   return telco
+def months_to_years(df):
+    df['tenure_years']=round(df.tenure//12).astype('category')
+    return df
 
 telco['tenure_years'] = telco.tenure//12
 telco
@@ -44,6 +49,20 @@ telco
 # variable, e.g. tenure_years with total_charges. For ideas on effective ways to visualize 
 # categorical with continuous: https://datavizcatalogue.com/. You can then look into seaborn 
 # and matplotlib documentation for ways to create plots.
+
+def plot_categorical_and_continuous_vars(df):
+    plt.figure(figsize=(16,8))
+    plt.subplot(1,3,1)
+    plt.bar(df.tenure_years,df.total_charges)
+    plt.xlabel('Tenure in years')
+    plt.ylabel('Total charges in dollars')
+    plt.subplot(1,3,2)
+    sns.stripplot(df.tenure_years,df.total_charges)
+    plt.subplot(1,3,3)
+    plt.scatter(df.tenure_years,df.total_charges)
+    plt.pie(df.groupby('tenure_years')['total_charges'].sum(),labels=list(df.tenure_years.unique()),autopct='%1.1f%%',shadow=True)
+    plt.title(" Percent of total charges by tenure")
+    plt.show()
 
 train, test = split_my_data(telco, train_ratio=.80, seed=123)
 train = train.drop('customer_id', axis=1)
