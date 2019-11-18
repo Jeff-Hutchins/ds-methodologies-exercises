5e791681be7eb0f7e8f3568a98b6679668d13c1c
Jeffrey Hutchins <jeffrey.w.hutchins@gmail.com>
2019-11-18 16:53:02 -0600

    updated

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -81,3 +81,15 @@ def get_opsd_data(use_cache=True):
     df.to_csv('opsd.csv', index=False)
     return df
 
+def get_fitbit_data():
+    df1 = pd.read_csv('2018-04-26_and_2018-05-26.csv', nrows=31)
+    df2 = pd.read_csv('2018-05-27_and_2018-06-26.csv', nrows=31)
+    df3 = pd.read_csv('2018-06-27_and_2018-07-27.csv', nrows=31)
+    df4 = pd.read_csv('2018-07-28_and_2018-08-26.csv', nrows=30)
+    df5 = pd.read_csv('2018-08-27_and_2018-09-26.csv', nrows=31)
+    df6 = pd.read_csv('2018-09-27_and_2018-10-27.csv', nrows=31)
+    df7 = pd.read_csv('2018-10-28_and-2018-11-27.csv', nrows=31)
+    df8 = pd.read_csv('2018-11-28_and_2018-12-28.csv', nrows=9)
+    df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8], join="inner", ignore_index=True)
+    return df
+
--- time_series/prepare.py
+++ time_series/prepare.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def prep_store_data(df):
     # parse the date column and set it as the index
@@ -19,3 +20,15 @@ def get_sales_by_day(df):
     sales_by_day = df.resample('D')[['sales_total']].sum()
     sales_by_day['diff_with_last_day'] = sales_by_day.sales_total.diff()
     return sales_by_day
+
+def get_fitbit_data(df):
+    df1 = pd.read_csv('2018-04-26_and_2018-05-26.csv', nrows=31)
+    df2 = pd.read_csv('2018-05-27_and_2018-06-26.csv', nrows=31)
+    df3 = pd.read_csv('2018-06-27_and_2018-07-27.csv', nrows=31)
+    df4 = pd.read_csv('2018-07-28_and_2018-08-26.csv', nrows=30)
+    df5 = pd.read_csv('2018-08-27_and_2018-09-26.csv', nrows=31)
+    df6 = pd.read_csv('2018-09-27_and_2018-10-27.csv', nrows=31)
+    df7 = pd.read_csv('2018-10-28_and-2018-11-27.csv', nrows=31)
+    df8 = pd.read_csv('2018-11-28_and_2018-12-28.csv', nrows=9)
+    df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8], join="inner", ignore_index=True)
+    return df
